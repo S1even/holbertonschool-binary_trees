c36cf2849e936afad2097a4f194dc82197881749
S1even <Stevendevin95520@hotmail.com>
2024-08-05 16:09:33 +0200

    Fix issue

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if perfect, 0 otherwise
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height, size, perfect_size;
+
+	if (tree == NULL)
+		return (0);
+
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
+	perfect_size = (1 << height) - 1;
+
+	return (size == perfect_size);
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: height of the tree, or 0 if tree is NULL
@@ -28,24 +48,4 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
-}
-
-/**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
- * Return: 1 if perfect, 0 otherwise
- */
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	size_t height, size, perfect_size;
-
-	if (tree == NULL)
-		return (0);
-
-	height = binary_tree_height(tree);
-	size = binary_tree_size(tree);
-	perfect_size = (1 << height) - 1;
-
-	return (size == perfect_size);
 }
