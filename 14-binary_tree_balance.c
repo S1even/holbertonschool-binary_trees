9bb4593620d58d096ca51778231836cee8cb657d
S1even <Stevendevin95520@hotmail.com>
2024-08-05 15:32:22 +0200

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,7 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree
- * Return: the balance factor, or 0 if tree is NULL
+ * Return: the balance factor, or 0 if tree is NULL.
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
